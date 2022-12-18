0009feb7c49e7e6919c91bad659b5476d23792e7
sunny liu <372930@gmail.com>
2022-12-18 23:00:59 +0900

    1218

---
+++ src/components/home/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HeaderSocials = () => {
+  return (
+    <div className="home_socials">
+      <a href="https://instagram.com/sunnymifune?igshid=Nzg3Njl1NG1=" className="home_social-link" target="_blank">
+      <i class="fa-brands fa-instagram"></i>
+      </a>
+    </div>
+  )
+}
+
+export default HeaderSocials
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import "./home.css";
-
+import Me from "../../assets/avatar-1.svg";
+import HeaderSocials from './HeaderSocials';
+import ScrollDowm from './ScrollDown';
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="home container" id="home">
+      <div className="intro">
+        <img src={Me} alt="" className="home_img" />
+        <h1 className="home_name">Sunny Mifune</h1>
+        <span className="home_education">I'm a Front-End developer</span>
+
+        <HeaderSocials />
+
+        <a href="#contact" className="btn">Hire Me</a>
+
+        <ScrollDowm />
+      </div>
+    </section>
   )
 }
 
