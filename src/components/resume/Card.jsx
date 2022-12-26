c0a33743c84a705248a88f3e60f65fb054aee330
sunny liu <372930@gmail.com>
2022-12-26 23:28:25 +0900

    add the position

--- src/components/resume/Card.jsx
+++ src/components/resume/Card.jsx
@@ -6,6 +6,7 @@ const Card = (props) => {
       <i className={props.icon}></i>
       <span className="timeline_date">{props.year}</span>
       <h3 className="timeline_title">{props.title}</h3>
+      <span className="timeline_posit">{props.posit}</span>
       <p className="timeline_text">{props.desc}</p>
     </div>
   )
--- src/components/resume/Data.jsx
+++ src/components/resume/Data.jsx
@@ -29,7 +29,8 @@ const Data = [
     icon: "icon-briefcase",
     year: "2021.09 - 2022.09",
     title: "Li & Fung Limited l Taipei, Taiwan",
-    desc: "Communicate and follow up with internal and external constituents, including clients, vendors, mills, and colleagues, to solve problems as soon as possible. Build and maintain close relationships with buyers in order to understand their goals and provide them with suitable sourcing services.",
+    posit: "Merchandiser (Pre-order Dept.)",
+    desc: "🔹Communicate and follow up with internal and external constituents, including clients, vendors, mills, and colleagues, to solve problems as soon as possible.\n 🔹 Build and maintain close relationships with buyers in order to understand their goals and provide them with suitable sourcing services.",
   },
   {
     id: 5,
@@ -37,6 +38,7 @@ const Data = [
     icon: "icon-briefcase",
     year: "2021.04 - 2021.09",
     title: "KindMinds Innovations l Taipei, Taiwan",
+    posit: "Logistic Assistant",
     desc: "Create sea or express shipment to send products oversea. Tracking packages if arrive on time and calculating the accounts receivable. Weekly checking the Amazon FBA system, sometime the system ",
   },
   {
@@ -45,6 +47,7 @@ const Data = [
     icon: "icon-briefcase",
     year: "2018.10 - 2020-06",
     title: "Li & Fung Limited l Taipei, Taiwan",
+    posit: "Assistant Merchandiser (Production Dept.)",
     desc: "Manage production for the children's accessories / active apparel industry, like monitoring the transit calendars, orders, production progress, and shipping to ensure deliveries.",
   },
 ];
