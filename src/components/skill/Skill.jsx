969f4ee0c74f969f661dbec70bebdc2a0ca74dab
sunny liu <372930@gmail.com>
2023-01-26 23:19:46 +0900

    try to make a column

--- src/components/skill/Skill.jsx
+++ src/components/skill/Skill.jsx
@@ -3,8 +3,28 @@ import "./skill.css";
 
 const Skill = () => {
   return (
-
-  )
+    <section className="skill container section" id='skill'>
+      <h2 className="section_title">Skills</h2>
+    <table>
+      <tr>
+        <td>
+        <p>HTML</p>
+        </td>
+        <td>
+          <p>🌟🌟🌟🌟</p>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <p>CSS</p>
+        </td>
+        <td>
+          <p>🌟🌟🌟🌟</p>
+        </td>
+      </tr>
+    </table>
+    </section>
+)
 }
 
 export default Skill
